775f0516f62acfea89c270c732a87be7ecb0d647
Hasib  Zunair <hasibzunair@gmail.com>
2019-07-18 05:05:59 +0600

    added notebooks

---
+++ helpers.py
@@ -0,0 +1,12 @@
+import os 
+
+def create_directory(directory):
+    '''
+    Creates a new folder in the specified directory if the folder doesn't exist.
+    INPUT
+        directory: Folder to be created, called as "folder/".
+    OUTPUT
+        New folder in the current directory.
+    '''
+    if not os.path.exists(directory):
+        os.makedirs(directory)
